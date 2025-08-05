8ce213a3a79b31f60de47825fd23922f92c7df3b
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-05 11:21:46 +0200

    feat: create controller and page for search the song with id

--- src/main/java/org/lessons/java/best_of_the_year/controller/PageController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controller/PageController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -33,6 +34,23 @@ public class PageController {
 
         model.addAttribute("songTitles", songTitles);
         return "song";
+    }
+
+    @GetMapping("/song/{id}")
+    public String getMethodName(@PathVariable int id, Model model) {
+        List<Song> songs = getBestSong();
+
+        Song song = songs.stream()
+                .filter(s -> s.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if (song != null) {
+            model.addAttribute("title", song.getTitle());
+        } else {
+            model.addAttribute("title", "Canzone non trovata");
+        }
+        return "song-by-id";
     }
 
     @GetMapping("/movie")
