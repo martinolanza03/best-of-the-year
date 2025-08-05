18157ca931bda96798bda2a27dc62fef6e8c065f
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-05 11:09:30 +0200

    feat: create controller and page for best song

--- src/main/java/org/lessons/java/best_of_the_year/controller/PageController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controller/PageController.java
@@ -1,5 +1,10 @@
 package org.lessons.java.best_of_the_year.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.lessons.java.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +23,23 @@ public class PageController {
 
     @GetMapping("/song")
     public String song(Model model) {
+
+        List<Song> songs = getBestSong();
+
+        String songTitles = songs.stream()
+                .map(Song::getTitle)
+                .collect(Collectors.joining(",  "));
+
+        model.addAttribute("songTitles", songTitles);
         return "song";
+    }
+
+    private List<Song> getBestSong() {
+        return Arrays.asList(
+                new Song("Call out my name", 1),
+                new Song("Starboy", 2),
+                new Song("Bene", 3),
+                new Song("Non ti vedo più", 4));
     }
 
 }
